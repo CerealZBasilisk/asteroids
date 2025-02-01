95354170978ce1cc84d5f96d72dc724b37cb2a27
CerealZBasilisk <93325934+CerealZBasilisk@users.noreply.github.com>
2025-02-01 12:06:03 -0500

    added circle shape and player classes, gave them the ability to move, created groups to handle drawing and calling of entities in game.

--- main.py
+++ main.py
@@ -3,17 +3,28 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
 	print(f"Starting asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	delta_timer = pygame.time.Clock()
+	dt = 0
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	Player.containers = (updatable, drawable)
+	player_main = Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
 	while True:
 		for event in pygame.event.get():
     			if event.type == pygame.QUIT:
         			return
 		pygame.Surface.fill(screen,(0,0,0))
+		updatable.update(dt)
+		for object in drawable:
+			object.draw(screen)
 		pygame.display.flip()
+		dt = delta_timer.tick(60)/1000
 
 
 if __name__ == "__main__":
